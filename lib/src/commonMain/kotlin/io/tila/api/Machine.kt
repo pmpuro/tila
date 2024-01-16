c7cc5fe0e5e6bed358dd2ae206e0d1bbea0ba7f1
Panu Puro <panu.puro@gmail.com>
2024-01-16 21:55:41 +0200

    Tie all together

--- lib/src/commonMain/kotlin/io/tila/api/Machine.kt
+++ lib/src/commonMain/kotlin/io/tila/api/Machine.kt
@@ -1,11 +1,22 @@
 package io.tila.api
 
 import io.tila.impl.Data
+import io.tila.impl.Derivator
+import io.tila.impl.EventLoop
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
 
 
-class Machine(data: DataMap = mapOf()) {
+class Machine(
+    data: DataMap = mapOf(),
+    coroutineScope: CoroutineScope = MainScope()
+) : Derive {
     private val appData = Data(data.toMutableMap())
-
+    private val derivativeApplier = ApplyDerivative { it(appData) }
+    private val derivator = Derivator(derivativeApplier)
+    private val eventHandlerApplier = ApplyEventHandler { handler, args -> handler(appData, args) }
+    private val eventLoop = EventLoop(coroutineScope, eventHandlerApplier)
+    override fun derive() = derivator.derive()
 }
 
 /*
